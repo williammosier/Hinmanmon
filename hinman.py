d371a50a06dfacfe9faa4d71166c57cab52d83ab
cwolosh1 <cwolosh1@binghamton.edu>
2018-04-12 17:34:26 -0400

    added 'smith' location; updated location dictionary

--- hinman.py
+++ hinman.py
@@ -22,33 +22,45 @@ class Hinman():
 		self.locs = {
 			"hinman college":\
 			location.Location(pygame.image.load('art/environment/hinman_college.png'),\
-							pygame.image.load('art/environment/hinman_college_mask.png').convert_alpha(),\
-							pygame.image.load('art/environment/hinman_college_loadzones.png'),'sound/music/death.ogg',-1900,-1200,2600,2600),
+			pygame.image.load('art/environment/hinman_college_mask.png').convert_alpha(),\
+			pygame.image.load('art/environment/hinman_college_loadzones.png'),\
+			'sound/music/death.ogg',-1900,-1200,3000,3000),
 
 			"success center":\
 			location.Location(pygame.image.load('art/environment/success_center.png'),\
-							pygame.image.load('art/environment/success_center_mask.png').convert_alpha(),\
-							pygame.image.load('art/environment/success_center_loadzones.png'),'sound/music/reslife.ogg',0,0,640,480),
+			pygame.image.load('art/environment/success_center_mask.png').convert_alpha(),\
+			pygame.image.load('art/environment/success_center_loadzones.png'),\
+			'sound/music/reslife.ogg',0,0,640,480),
 
 			"hughes":\
 			location.Location(pygame.image.load('art/environment/hughes.png'),\
-							pygame.image.load('art/environment/hughes_mask.png').convert_alpha(),\
-							pygame.image.load('art/environment/hughes_loadzones.png'),'sound/music/reslife.ogg',0,0,2000,500),
+			pygame.image.load('art/environment/hughes_mask.png').convert_alpha(),\
+			pygame.image.load('art/environment/hughes_loadzones.png'),\
+			'sound/music/reslife.ogg',0,0,2000,500),
 
 			"cleveland":\
 			location.Location(pygame.image.load('art/environment/cleveland.png'),\
-							pygame.image.load('art/environment/cleveland_mask.png').convert_alpha(),\
-							pygame.image.load('art/environment/cleveland_loadzones.png'),'sound/music/reslife.ogg',0,0,2000,500),
+			pygame.image.load('art/environment/cleveland_mask.png').convert_alpha(),\
+			pygame.image.load('art/environment/cleveland_loadzones.png'),\
+			'sound/music/reslife.ogg',0,0,2000,500),
 
 			"lehman":\
 			location.Location(pygame.image.load('art/environment/lehman.png'),\
-							pygame.image.load('art/environment/lehman_mask.png').convert_alpha(),\
-							pygame.image.load('art/environment/lehman_loadzones.png'),'sound/music/reslife.ogg',0,0,2000,500),
+			pygame.image.load('art/environment/lehman_mask.png').convert_alpha(),\
+			pygame.image.load('art/environment/lehman_loadzones.png'),\
+			'sound/music/reslife.ogg',0,0,2000,500),
 
 			"roosevelt":\
 			location.Location(pygame.image.load('art/environment/roosevelt.png'),\
-							pygame.image.load('art/environment/roosevelt_mask.png').convert_alpha(),\
-							pygame.image.load('art/environment/roosevelt_loadzones.png'),'sound/music/reslife.ogg',0,0,2000,500)
+			pygame.image.load('art/environment/roosevelt_mask.png').convert_alpha(),\
+			pygame.image.load('art/environment/roosevelt_loadzones.png'),\
+			'sound/music/reslife.ogg',0,0,2000,500),
+
+			"smith":\
+			location.Location(pygame.image.load('art/environment/smith.png'),\
+			pygame.image.load('art/environment/smith_loadzones.png').convert_alpha(),\
+			pygame.image.load('art/environment/smith_mask.png'),\
+			'sound/music/reslife.ogg',0,0,2000,500)
 			}
 
 		char = {
